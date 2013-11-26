f5ab6aa87b00d93b591d00355ee5c6bc0c7862a0
Eren <eren@Eren-MacBook-Air.local>
2013-11-26 06:32:45 +0200

    Tekli Dosya Paylaşım Hiyerarşisi Eklendi.
    Dosya upload işleminde alt nodelar da bulunan departman ile paylaşılan
    döküman otomatik olarak üst departmanlar ilede paylaşım işlemi yapıldı.
    Tek departman için doğru çalışmakta. Üst departman node da bulunanda
    eklenince duplicate oluyor.

--- E-DMS/src/java/com/erenerdogan/service/GroupSharedDaoImpl.java
+++ E-DMS/src/java/com/erenerdogan/service/GroupSharedDaoImpl.java
@@ -19,8 +19,8 @@ import javax.persistence.Persistence;
  *
  * @author eren
  */
-public class GroupSharedDaoImpl implements GroupSharedDaoInterface{
-    
+public class GroupSharedDaoImpl implements GroupSharedDaoInterface {
+
     private EntityManagerFactory emf;
     private EntityManager em;
 
@@ -32,10 +32,10 @@ public class GroupSharedDaoImpl implements GroupSharedDaoInterface{
     @Override
     public List<GroupShared> getMyGroupShared(int id) {
         List<Groups> groups = new GroupsDaoImpl().getUserGroups(id);
-        
+
         List<GroupShared> groupShareds = new ArrayList<GroupShared>();
-        
-        for (Groups g : groups) {            
+
+        for (Groups g : groups) {
             for (GroupShared gs : g.getGroupSharedCollection()) {
                 groupShareds.add(gs);
             }
@@ -44,16 +44,32 @@ public class GroupSharedDaoImpl implements GroupSharedDaoInterface{
     }
 
     @Override
-    public void addGroupsShared(Files file,Groups groupID) {
-        
+    public void addGroupsShared(Files file, Groups groupID) {
+
         GroupShared gs = new GroupShared();
         gs.setGsfid(file);
         gs.setGsgid(groupID);
+
         EntityTransaction et = em.getTransaction();
         et.begin();
-        em.persist(gs);
+        if (groupID.getGid().equals(groupID.getGsubid())) {
+            em.persist(gs);
+        } else {
+            List<GroupShared> l = new ArrayList<GroupShared>();
+            l.add(gs);
+            do {
+                groupID = em.find(Groups.class, groupID.getGsubid());
+                GroupShared gs1 = new GroupShared();
+                gs1.setGsfid(file);
+                gs1.setGsgid(groupID);
+                l.add(gs1);
+            } while (!groupID.getGid().equals(groupID.getGsubid()));
+            
+            for (GroupShared groupShared : l) {
+                em.persist(groupShared);
+            }
+        }
         et.commit();
         em.close();
     }
-    
 }
